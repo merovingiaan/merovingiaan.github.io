d5770af6984bf37e16d20e6edb1d9ee528c1be33
merovingiaan <merovingiaan@merovingiaanui-MacBook-Pro.local>
2016-03-02 20:23:35 +0900

    add profile page

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
+    $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: GenericViewCtrl});
     $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
     $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
-    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
+    $routeProvider.when('/guestbook', {templateUrl: 'partials/guestbook.html', controller: GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
